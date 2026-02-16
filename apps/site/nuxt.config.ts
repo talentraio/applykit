94f29998a0cac45b54b9fb78c1c95e8408d8813f
Alex Guzey <alex.guzey@gmail.com>
2026-02-17 00:11:40 +0100

    chore(site): add favicon set and head links

--- apps/site/nuxt.config.ts
+++ apps/site/nuxt.config.ts
@@ -27,6 +27,18 @@ export default defineNuxtConfig({
     '@vueuse/nuxt'
   ],
 
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-16x16.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/favicon/site.webmanifest.json' }
+      ]
+    }
+  },
+
   fonts: {
     families: [
       {
